5f2c4f65c300ae100ea1b83d937587de8304252c
YumnaAsim <yumnaasim11@gmail.com>
2017-11-18 02:06:44 +0500

    add new pages

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-
+var about = require('./routes/about');
+var register = require('./routes/register');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-
+app.use('/about', about);
+app.use('/register', register);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
