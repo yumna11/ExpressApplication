b0f926b11f74ebf54b5294bd322392b1880dcfd8
YumnaAsim <yumnaasim11@gmail.com>
2017-11-24 00:40:43 +0500

    json retrival

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var register = require('./routes/register');
 
 var app = express();
 
+app.locals.subscribers = "1200";
+app.locals.videoData = require('./videodata.json');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/about', about);
 app.use('/register', register);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var videoData = require('../videodata.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {projectName:'Video Champ'});
+  res.render('index', {projectName:'Video Champ',
+                        vd:videoData});
 });
 
 module.exports = router;
